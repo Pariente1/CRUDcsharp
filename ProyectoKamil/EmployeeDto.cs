ace0b2af09958127014e03be847b9cde23c852fe
Pariente <neodante12@gmail.com>
2025-04-01 16:34:27 -0700

    cambiamos EmployeeDto a public

--- ProyectoKamil/EmployeeDto.cs
+++ ProyectoKamil/EmployeeDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProyectoKamil
 {
-    internal class EmployeeDto
+    public class EmployeeDto
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
